a2d2317568812e80ec58c1f42f125f55c090d5ce
Jzaroli <jzaroli@gmail.com>
2025-01-19 15:35:08 -0700

    added two projects and fixed CSS

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -60,9 +60,13 @@ const styles = {
         marginLeft: '1vw',
         fontSize: '3vw'
     },
-    paragraph: {
+    paragraph1: {
         fontSize: '1rem',
-        margin: '1vw',
+        marginTop: '1vw',
+        textIndent: '45px',
+        },
+    paragraph2: {
+        fontSize: '1rem',
         textIndent: '45px',
         }
 }
@@ -77,9 +81,12 @@ function About() {
                     </div>
                     <div style={styles.div2}> 
                         <h1 style={styles.h1}>Johann Zaroli</h1>
-                        <h2 style={styles.h2}>Software Developer</h2>
-                        <p style={styles.paragraph}>
-                        As a software developer, I specialize in delivering web apps that combine intuitive design, business process expertise, and high performance. Drawing from my STEM background, I approach algorithmic challenges with logical and programmatic thinking. My fintech experience helps me empathize with customer needs, plan for costs and scalability, and adapt to evolving markets and technologies. Having worked at both unicorn start-ups and leading software firms, I’ve learned the value of clear communication, the power of collaboration, and the joy of mentorship. Feel free to explore my portfolio and resume, and reach out through the contact section if you'd like to work together. Thank you for stopping by!
+                        <h2 style={styles.h2}>Full Stack Web Developer</h2>
+                        <p style={styles.paragraph1}>
+                            I specialize in delivering full-stack solutions that combine intuitive design, business process expertise, and high performance. Drawing from my STEM background, I approach algorithmic challenges with logical and programmatic thinking. My fintech experience helps me empathize with customer needs, plan for costs and scalability, and adapt to evolving markets and technologies. Having worked at both unicorn start-ups and leading software firms, I’ve learned the value of clear communication, the power of collaboration and the joy of mentorship.
+                        </p>
+                        <p style={styles.paragraph2}>
+                            I’m looking for a team who’s open to taking on a more junior developer who’s ramping incredibly fast, delivering high quality work with all of the technologies listed below, and who has been at the top of his bootcamp. If I had to pick, I’d say back-end development comes naturally to me. There is also much I can offer from my GTM background in tech to a warm and collaborative personality that has the team and the company’s best interest in mind. Feel free to explore my portfolio and resume! Thank you.
                         </p>
                     </div>
                 </div>
